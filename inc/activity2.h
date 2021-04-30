a1edeeffa3feb72ec44cb9f08eef43095ab34fda
Saijoshitha <50612828+Saijoshitha@users.noreply.github.com>
2021-04-30 17:18:49 +0530

    Create activity2.h

---
+++ inc/activity2.h
@@ -0,0 +1,22 @@
+#ifndef __ACTIVITY1_H_
+#define __ACTIVITY_H_
+#include <avr/io.h>
+
+#define F_CPU	20000000
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+
+#define F_CPU	20000000
+
+#include <avr/io.h>
+#include<util/delay.h>
+#include <avr/interrupt.h>
+
+void InitADC();
+
+
+#endif 

a01fc0d1e7232be43aeb31d4c8857b21ea15cc91
Saijoshitha <50612828+Saijoshitha@users.noreply.github.com>
2021-04-30 17:24:33 +0530

    Update Activity1.c

--- src/Activity1.c
+++ src/Activity1.c
@@ -1,11 +1,35 @@
-#include "Activity1.h"
-
-
-void delay_ms(uint32_t delay_time)
+#include "activity1.h"
+/**
+ * @brief Initialize all the Peripherals and pin configurations
+ * 
+ */
+void peripheral_init(void)
 {
-	uint32_t units = 0;
-	for (units = 0; units <= delay_time; units++)
-	{
-		_delay_ms(1);
-	}
+	/* Configure LED Pin */
+	DDRB |=(1<<LED_PIN);
+    DDRD &=~(1<<BUTTONSENSOR);
+    DDRD &=~(1<<TEMPSENSOR);
+    SENSOR|=(1<<BUTTONSENSOR);
+    SENSOR|=(1<<TEMPSENSOR);
+}
+/**
+ * @brief Decides whether the tempsensor is to be considered
+ * 
+ */
+void tempbuttonSensor()
+{
+    
+    if((PIND&(1<<PD0)) && (PIND&(1<<PD1)))/**< if both buttonsensor and tempsensor reads HIGH */
+       {
+           uint16_t temp;
+           PORTB|=(1<<PB0);
+           temp=Read_ADC(0);/**< Read tempsensor value */
+           GeneratePWM(temp);
+           delay_ms(200);
+       }
+       else
+       {
+           PORTB&=~(1<<PB0);
+           delay_ms(200);
+       }
 }

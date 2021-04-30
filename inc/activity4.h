d05893d9c4589a1bf7d9a7807abe832c5cf43976
Saijoshitha <50612828+Saijoshitha@users.noreply.github.com>
2021-04-30 11:49:35 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,13 +1,4 @@
-/**
- * @file activity4.h
- * @author Dhanush U
- * @brief header file for activity3
- * @version 0.1
- * @date 2021-04-28
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #ifndef __ACTIVITY4_H__
 #define __ACTIVITY4_H__
 #include "project_config.h"

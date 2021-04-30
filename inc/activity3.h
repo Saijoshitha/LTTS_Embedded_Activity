0f31ec680546e805be61c8769338570924a4a1d6
Saijoshitha <50612828+Saijoshitha@users.noreply.github.com>
2021-04-30 17:19:48 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -4,13 +4,10 @@
 #include <avr/io.h>
 #include "user_utils.h"
 #include "activity4.h"
-/**
- * @brief Generate PWM values for given digital Input
- * 
- */
 
-void Init_PWM(void);/**< Initialize all the Peripherals and pin configurations for PWM */
-void GeneratePWM(uint16_t ADC_value);/**< perform PWM operations on ADC value*/
+
+void Init_PWM(void);
+void GeneratePWM(uint16_t ADC_value);
 
 
 #endif  

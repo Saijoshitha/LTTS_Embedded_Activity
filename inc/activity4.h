6e716f188dbbc426335276bb92082c20026fd940
Saijoshitha <50612828+Saijoshitha@users.noreply.github.com>
2021-04-30 18:21:54 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -2,10 +2,7 @@
 #ifndef __ACTIVITY4_H__
 #define __ACTIVITY4_H__
 #include "project_config.h"
-/**
- * @brief Send data via UART interface
- * 
- */
+
 
 void Init_USART();/**< Initialize all the Peripherals and pin configurations for PWM */
 int USARTRead();/**< perform PWM operations on ADC value*/

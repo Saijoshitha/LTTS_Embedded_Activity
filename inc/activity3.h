3adb6470dc7778a3d01d56a791fbff533f8531a2
Saijoshitha <50612828+Saijoshitha@users.noreply.github.com>
2021-04-30 11:49:11 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -1,13 +1,4 @@
-/**
- * @file activity3.h
- * @author Dhanush U
- * @brief header file for activity3
- * @version 0.1
- * @date 2021-04-28
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #ifndef __ACTIVITY3_H__
 #define __ACTIVITY3_H__
 #include <avr/io.h>
